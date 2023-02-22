834a64532c589f4822fd56d9577f9f030942cb5c
B C <118926353+Kzreaves@users.noreply.github.com>
2023-02-22 16:11:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import random
+# import my_module
+
+# random = random.randint(1, 10)
+# print(random)
+
+# print(my_module.pi)
+
+
+# random_float = random.random()
+# print(random_float)
+
+
+
+
+
+
+
+
+
+# random = random.randint(0, 2)
+
+# if random == 1:
+#   print("Heads")
+# elif random == 2:
+#   print("Tails")
+
+# Import the random module here
+
+# import random
+# names_string = input("Give me everybody's names, separated by a comma. ")
+# names = names_string.split(", ")
+# choice = random.choice(names)
+# print(choice)
+
+
+names = input("List of names:") 
+names = names.split(", ")
+index = random.randint(0, len(names) -1 ) 
+choice = names[index]
+print(choice) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
